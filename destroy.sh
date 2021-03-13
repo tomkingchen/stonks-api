f69952865cb96105c8bb7d989be1ee0a1b55919d
Tom Chen <tomking@gmail.com>
2021-03-14 09:58:53 +1100

    Add delete stack script

---
+++ destroy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#set -x
+
+stack_name="stonks-api-stack"
+region="ap-southeast-2"
+
+if ! aws cloudformation describe-stacks --region $region --stack-name $stack_name; then
+	echo "Stack does not exist."
+else
+	type_formation='delete-stack'
+	echo "Begin "$type_formation"......."
+	eval "aws cloudformation $type_formation --stack-name $stack_name --region $region"
+fi
+
+exit

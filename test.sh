98e3a455e0e344360e9edb99a672486bc045a299
Tom Chen <tomking@gmail.com>
2021-03-14 11:45:10 +1100

    Add API Key requirement

--- test.sh
+++ test.sh
@@ -9,12 +9,19 @@ base_uri="${base_uri%\"}"
 base_uri="${base_uri#\"}"
 base_uri+='/trade'
 
+api_key=$(aws apigateway get-api-keys --name-query 'stonks-api-key' --include-value|jq '.items[0].value')
+# Remove quotes around the api key
+api_key="${api_key%\"}"
+api_key="${api_key#\"}"
+api_key="x-api-key:$api_key"
+
 # Add stonk trade into API
 curl --request POST $base_uri \
+--header $api_key \
 --header 'Content-Type: application/json' \
 --data-raw '{
     "company": "GME",
-    "action": "sell",
+    "action": "buy",
     "price": 500,
     "volume": 999
 }'
@@ -22,5 +29,5 @@ curl --request POST $base_uri \
 # List stonk trades from API
 echo ""
 echo "Trades you have done:"
-curl --request GET $base_uri |jq
+curl --request GET $base_uri --header $api_key |jq
 exit
